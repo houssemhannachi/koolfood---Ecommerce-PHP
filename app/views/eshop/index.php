e4ebd69f840406863d1733aa156f4cfaede73ac5
Houssem Hannachi <houssem.hannachi@enis.tn>
2022-01-27 22:40:28 +0100

    Update index.php

--- app/views/eshop/index.php
+++ app/views/eshop/index.php
@@ -1,7 +1,6 @@
 <?php $this->view("header", $data); ?>
 
 <?php $this->view("slider", $data); ?>
-
 <section>
 	<div class="container">
 		<div class="row">
@@ -24,48 +23,54 @@
 					<?php //show($segment_data)
 					?>
 				</div>
-				<!--features_items-->
 
-				<?php if (isset($segment_data) && is_array($segment_data)) : $num = 0 ?>
-
-					<div class="category-tab">
-						<!--category-tab-->
-						<div class="col-sm-12">
-							<ul class="nav nav-tabs">
-								<?php foreach ($segment_data as $key => $seg) : $num++ ?>
-									<li <?= ($num == 1) ? 'class="active"' : ''; ?>><a href="#<?= $key ?>" data-toggle="tab"><?= $key ?></a></li>
-								<?php endforeach; ?>
-							</ul>
-						</div>
-						<div class="tab-content">
-
-							<?php $num = 0 ?>
-							<?php foreach ($segment_data as $key => $seg) : $num++ ?>
-
-								<div class="tab-pane fade <?= ($num == 1) ? ' active in ' : ''; ?> " id="<?= $key ?>">
-
-									<div class="col-sm-10">
-
-										<?php if (is_array($seg)) : ?>
-											<?php foreach ($seg as $row) : ?>
-
-												<?php $this->view("product.inc", $row); ?>
-
-											<?php endforeach; ?>
-										<?php endif; ?>
-
-									</div>
-
-								</div>
-							<?php endforeach; ?>
-						</div>
-					</div>
-					<!--/category-tab-->
-				<?php endif; ?>
-
-				<?php Page::show_links() ?>
 			</div>
 		</div>
+		<div> <img src="<?= ASSETS . THEME ?>images/home/adv.jpg?v=<?php echo time(); ?>" width="100%" /></div>
+
+		<div class="col-sm-9 padding-right">
+			<!--features_items-->
+
+			<?php if (isset($segment_data) && is_array($segment_data)) : $num = 0 ?>
+
+				<div class="category-tab">
+					<!--category-tab-->
+					<div class="col-sm-12">
+						<ul class="nav nav-tabs">
+							<?php foreach ($segment_data as $key => $seg) : $num++ ?>
+								<li <?= ($num == 1) ? 'class="active"' : ''; ?>><a href="#<?= $key ?>" data-toggle="tab"><?= $key ?></a></li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+					<div class="tab-content">
+
+						<?php $num = 0 ?>
+						<?php foreach ($segment_data as $key => $seg) : $num++ ?>
+
+							<div class="tab-pane fade <?= ($num == 1) ? ' active in ' : ''; ?> " id="<?= $key ?>">
+
+								<div class="col-sm-10">
+
+									<?php if (is_array($seg)) : ?>
+										<?php foreach ($seg as $row) : ?>
+
+											<?php $this->view("product.inc", $row); ?>
+
+										<?php endforeach; ?>
+									<?php endif; ?>
+
+								</div>
+
+							</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
+				<!--/category-tab-->
+			<?php endif; ?>
+
+			<?php Page::show_links() ?>
+		</div>
+	</div>
 	</div>
 </section>
 
