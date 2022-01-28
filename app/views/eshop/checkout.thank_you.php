a14c6a68585b9bf34966846006960ebd62b53c36
Houssem Hannachi <houssem.hannachi@enis.tn>
2022-01-28 16:38:13 +0100

    Update checkout.thank_you.php

--- app/views/eshop/checkout.thank_you.php
+++ app/views/eshop/checkout.thank_you.php
@@ -1,6 +1,20 @@
 <?php $this->view("header", $data); ?>
 <div style="text-align: center;">
-
+<br><br>
+<div class="pizzaro-order-steps">
+			<ul>
+				<li>
+					<span class="step">1</span> Shopping Cart 
+				</li>
+				<li>
+					<span class="step">2</span> Checkout
+				</li>
+				<li  class="cart">
+					<span class="step" >3</span> Order Complete
+				</li>
+			</ul>
+		</div>
+		<div class="entry-header"> </div>
 	<h1>Thank you for shopping with us!</h1>
 	<h4>Your order was successful</h4>
 	<br><br>
